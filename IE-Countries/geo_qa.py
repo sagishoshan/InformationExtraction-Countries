6b92a6c09a241b23b9c14ff03e56ac48480cbb36
sagishoshan <sagishoo@gmail.com>
2020-07-04 16:08:55 +0300

    setting the ground

---
+++ IE-Countries/geo_qa.py
@@ -0,0 +1,12 @@
+import sys
+
+command = sys.argv[1]
+given_input = sys.argv[2]
+print("command is", command)
+print("input is", given_input)
+
+if command == "create":
+    print("case1")
+
+if command == "question":
+    print("case2")
